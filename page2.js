d7377616ad4e88d03a4411b89e895e68c530d317
LIEUL <leh9111@naver.com>
2024-07-03 17:07:17 +0900

    feat: 슬라이드

--- page2.js
+++ page2.js
@@ -12,6 +12,7 @@ const DATA = {
 }
 
 Object.assign(self,{
+    onpointerdown(){},
     //main button 컴포넌트
     comp_mainBtn(_){
     const $ = html `<button class="ℓ mainBtn"></button>`;
@@ -115,8 +116,30 @@ Object.assign(self,{
     },
     //card 형태의 right section
     comp_card(_){
-     const $ = html`<section class="card">card</section>`;
-     const apply = ( ) => {  } 
+     const $ = html`<section class="card "><div class=slide><div class="slideContainer ℓ"><div class=imgContainer><img src=${IMG_DIR}/slide_makingCocktail_01.jpg alt=칵테일제조사진><img src=${IMG_DIR}/slide_makingCocktail_02.jpg alt=칵테일제조사진><img src=${IMG_DIR}/slide_makingCocktail_03.jpg alt=칵테일제조사진><img src=${IMG_DIR}/slide_makingCocktail_04.jpg alt=칵테일제조사진><img src=${IMG_DIR}/slide_makingCocktail_05.jpg alt=칵테일제조사진> </div></div> <div class=progressBar><img src=${IMG_DIR}/line1.svg><img class=progressLine src=${IMG_DIR}/line2.svg></div></div></section>`;
+     const $$ = ℓ($);
+     let startX, dx, tx = 0; //startX 마우스 클릭 포인트
+
+     function slideMoveHandler({x, y}){
+        dx = x - startX;  //dx 움직인 거리
+        update($$, {style:{transform:`translateX(${tx + dx}px)`}});
+     }
+     const apply = ( ) => {
+        update($$, {
+            onpointerdown(e){ //누르고 있는 상태
+                ({x :startX} = e);
+                console.log(startX);
+                addEventListener('pointermove', slideMoveHandler); //누르고 마우스를 움직였을때
+
+                addEventListener('pointerup', () => {
+                    tx = tx + dx;
+                    removeEventListener('pointermove', slideMoveHandler);
+                }, {once: true});
+                // startX =dx;
+            }
+        });
+        // startX =dx;
+     } 
      return comp(_,$,apply);
     },
     //recruit section
@@ -160,9 +183,61 @@ Object.assign(self,{
         return comp(_,$,apply);
     },
     comp_recruit_content(_){
-        const $ = html`<dl class="service"><dt class="subTitle_20">제공 서비스</dt><dd >핸드드립 원데이 클래스 + 디져트 3종 택1</dd></dl><dl class="keyword"><dt class="subTitle_20 ">키워드</dt><dd>
-        <></dd></dl><dl class="guide"><dt class="subTitle_20">안내</dt><dd><p class="danger">직접 매장에 예약하여 방문 후 리뷰를 작성하는 캠페인입니다</p><span>-타인의 대리 체험이 불가능합니다.</span> 
-        <p><h3>1.방문 안내🙇‍♂️</h3></p></dd></dl>`;
+        const $ = html`
+        <dl class="service">
+        <dt class="subTitle_20">제공 서비스</dt>
+        <dd >핸드드립 원데이 클래스 + 디져트 3종 택1</dd></dl>
+        <dl class="keyword"><dt class="subTitle_20 ">키워드<img src=${IMG_DIR}/copyicon.svg alt=키워드></dt><dd>
+        <></dd></dl>
+        
+        <dl class="guide">
+        <dt class="subTitle_20">안내</dt>
+        <dd>
+        <p class="danger">직접 매장에 예약하여 방문 후 리뷰를 작성하는 캠페인입니다</p>
+       
+        <div class="announcement">
+        뷰어 발표기간 <span>6.20 - 6.25</span>
+        </div>
+       
+        <div class="date">
+        <h3>참여 날짜 및 시간대</h3>
+
+        <div class="dateContent">
+        <div>
+         <span>6월24일</span>    <span>6월25일</span>    <span>6월26일</span>    <span>6월27일</span> <span>6월28일</span>
+        </div>
+        <ul>
+        <li>-1타임 오전 10:00 - 12:00 (총 2시간)</li>
+        <li>-2타임 오후 14:00 - 16:00 (총 2시간)</li>
+        </ul>
+        *날짜 선택후 1타임과 2타임 중 택 1
+        </div>
+        </div>
+       
+        <p class ="coffee">
+        <h3>☕️커피 기본 교육☕️</h3>
+        내가 만들어보는 라떼와 아메리카노
+        <ul>
+        <li>라떼 스티밍 체험 </li>
+        <li>에스프레소를 추출 및 시음하는 아메리카노 & 라떼 체험</li>
+        </ul>
+        </p>
+
+        <p>
+        <h3>논알콜로 즐기는 홈파티</h3>
+        논알콜 하이볼 시리즈! 논알콜 위스키 시럽을 활용한 음료 제조 체험
+        <ul>
+        <li>기본 시연 및 시음으로 3가지 음료</li>
+        <li>재료들을 나열해두고 자기만의 하이볼 만들어서 모두와 함께 맛보고 의견 나누기</li>
+        </ul>
+        </p>
+
+        <p>
+        <h3>주차 지원 : 3시간</h3>
+        신청시 한팀당 최대 2명(신청자 포함)입니다.🥰
+        </p>
+
+        </dd></dl>`;
          const $$ = ℓ($,
             loop(
              html`<span class ="tag ℓ"></span>`,
